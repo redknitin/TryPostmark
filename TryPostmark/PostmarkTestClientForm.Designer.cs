ff5a901a59697a635b41d658de3727bec78586e3
Nitin Reddy <redknitin@gmail.com>
2015-09-22 10:07:48 +0400

    Initial commit

---
+++ TryPostmark/PostmarkTestClientForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace TryPostmark
+{
+    partial class PostmarkTestClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProxy = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.ckDefaultCred = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Proxy Host";
+            // 
+            // txtProxy
+            // 
+            this.txtProxy.Location = new System.Drawing.Point(100, 16);
+            this.txtProxy.Name = "txtProxy";
+            this.txtProxy.Size = new System.Drawing.Size(172, 20);
+            this.txtProxy.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(100, 45);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(172, 20);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(100, 76);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(172, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "From";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(100, 131);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(172, 20);
+            this.txtFrom.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "To";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(100, 164);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(172, 20);
+            this.txtTo.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Subject";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(100, 197);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(172, 20);
+            this.txtSubject.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 231);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Body";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(100, 228);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(172, 122);
+            this.txtBody.TabIndex = 13;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(100, 388);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 14;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(181, 388);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // ckDefaultCred
+            // 
+            this.ckDefaultCred.AutoSize = true;
+            this.ckDefaultCred.Location = new System.Drawing.Point(100, 102);
+            this.ckDefaultCred.Name = "ckDefaultCred";
+            this.ckDefaultCred.Size = new System.Drawing.Size(115, 17);
+            this.ckDefaultCred.TabIndex = 16;
+            this.ckDefaultCred.Text = "System Credentials";
+            this.ckDefaultCred.UseVisualStyleBackColor = true;
+            // 
+            // PostmarkTestClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 423);
+            this.Controls.Add(this.ckDefaultCred);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProxy);
+            this.Controls.Add(this.label1);
+            this.Name = "PostmarkTestClientForm";
+            this.Text = "Postmark Test Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProxy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckBox ckDefaultCred;
+    }
+}
+
---
+++ TryPostmark/PostmarkTestClientForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TryPostmark
+{
+    public partial class PostmarkTestClientForm : Form
+    {
+        public PostmarkTestClientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendEmail() {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                wc.Headers["X-Postmark-Server-Token"] = "72a4ecea-d586-418e-8ec1-79b6ba6a11c5";
+
+                string fromAddr = "nithin.reddy@intertecsys.com";
+                string toAddr = "nithin.reddy@intertecsys.com";
+                string subject = "Test subject";
+                string body = "Test mail body";
+                string Data = "{" + string.Format("From: '{0}', To: '{1}', Subject: '{2}', TextBody: '{3}'", fromAddr, toAddr, subject, body) + "}";
+                string url = "https://api.postmarkapp.com/email";
+
+                wc.UploadString(url, Data);
+
+                MessageBox.Show("Sent");
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    if (!string.IsNullOrEmpty(txtProxy.Text))
+                    {
+                        WebProxy webProxy = new WebProxy(txtProxy.Text);
+                        if (ckDefaultCred.Checked)
+                        {
+                            webProxy.UseDefaultCredentials = true;
+                        }
+                        else
+                        {
+                            if (!string.IsNullOrEmpty(txtUsername.Text))
+                            {
+                                webProxy.Credentials = new NetworkCredential(txtUsername.Text, txtPassword.Text);
+                                webProxy.UseDefaultCredentials = false;
+                            }
+                        }
+                        wc.Proxy = webProxy;
+                    }
+
+                    wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    wc.Headers["X-Postmark-Server-Token"] = "72a4ecea-d586-418e-8ec1-79b6ba6a11c5";
+
+                    string fromAddr = txtFrom.Text;
+                    string toAddr = txtTo.Text;
+                    string subject = txtSubject.Text;
+                    string body = txtBody.Text;
+                    string Data = "{" + string.Format("From: '{0}', To: '{1}', Subject: '{2}', TextBody: '{3}'", fromAddr, toAddr, subject, body) + "}";
+                    string url = "https://api.postmarkapp.com/email";
+
+                    wc.UploadString(url, Data);
+
+                    MessageBox.Show("Sent");
+                }
+            }
+            catch (WebException ex)
+            {
+                using (var sr = new StreamReader(ex.Response.GetResponseStream()))
+                {
+                    var data = sr.ReadToEnd();
+                    //throw new ApplicationException(data);
+                    MessageBox.Show(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
